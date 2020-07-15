0745baffc91e76aa04c9da12c181b52b1f48f2ce
kramarenkodp <kramarenkodd@gmail.com>
2020-07-15 21:57:14 +0300

    Added home work 1

---
+++ HW1/src/Example3.java
@@ -0,0 +1,11 @@
+public class Example3 {
+    public static void main(String [] args) {
+        int days [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        for (int i = 0; i < days.length; i++) {
+            System.out.println(days[i]);
+        }
+        for (int i = 0; i < days.length; i++) {
+            System.out.print(days[i]);
+        }
+}
+}
